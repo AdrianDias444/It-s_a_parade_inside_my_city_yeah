a8093dcc4e5d731ed4186323acc925eac88228cb
Adrian Dias <adriandias0709@gmail.com>
2026-07-27 23:24:20 +0100

    memset

---
+++ libft/ft_memset.zig
@@ -0,0 +1,27 @@
+const lib = @import("lib.zig");
+
+// Used pub for this function could be used for any
+// file that import "lib.zig" module
+// with @import("lib.zig");
+
+pub fn ft_memset(s: [*]u8, c: i32, n: usize) [*]u8
+{
+    const byte: u8 = @truncate(c);
+    var i: usize = 0;
+
+    while(i < n) : (i+= 1) {
+        s[i] = byte;
+    }
+    return (s);
+}
+
+
+
+
+pub fn main() void
+{
+    // need to implement str_dup, for base this to
+    // memset, string are const, can't be modified
+
+    lib.print("{s}\n", .{ft_memset(str_ptr, 48, 3)});
+}
--- libft/lib.zig
+++ libft/lib.zig
@@ -4,6 +4,7 @@ const ft_isdigit_file = @import("ft_isdigit.zig");
 const ft_isascii_file = @import("ft_isascii.zig");
 const ft_isprint_file = @import("ft_isprint.zig");
 const ft_strlen_file = @import("ft_strlen.zig");
+const ft_memset_file = @import("ft_memset.zig");
 
 // Created a alias for all of functions creates
 // Now if we import the module "lib.zig"
@@ -14,6 +15,7 @@ pub const ft_isdigit = ft_isdigit_file.ft_isdigit;
 pub const ft_isascii = ft_isascii_file.ft_isascii;
 pub const ft_isprint = ft_isprint_file.ft_isprint;
 pub const ft_strlen = ft_strlen_file.ft_strlen;
+pub const ft_memset = ft_memset_file.ft_memset;
 
 // Created a alias for print
 // Because std.debug.print is so boring
