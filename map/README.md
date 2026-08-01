8b80bca932b9b21a6f62c4e95b311a4f292d1f46
Adrian Dias <adriandias0709@gmail.com>
2026-08-01 21:11:22 +0100

    update

--- map/README.md
+++
@@ -1,8 +0,0 @@
-
-
-
-
-Compile time x Run time Compilation
-
- 
-https://ziggit.dev/t/best-practices-writing-to-stdout/2760
--- map/main.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-pub fn main() !void {
-    const out = std.io.getStdOut().writer();
-    try out.print("Stuff\n", .{});
-
-}
